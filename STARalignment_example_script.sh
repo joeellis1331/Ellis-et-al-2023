71a9d7ed9c7ecf79ac5c986d5c609001f8fb11d5
Joseph Ellis <61436184+joeellis1331@users.noreply.github.com>
2023-03-26 19:17:27 -0400

    Update and rename STARalignment_example_script.txt to STARalignment_example_script.sh

--- STARalignment_example_script.sh
+++ STARalignment_example_script.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=ddHEK_coreg_align    # Job name
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=j.ellis@ufl.edu     # Where to send mail
+#SBATCH --ntasks=1                   # Run a single task
+#SBATCH --cpus-per-task=6            # Number of CPU cores per task
+#SBATCH --mem=100gb                     # Job memory request
+#SBATCH --time=24:00:00               # Time limit hrs:min:sec
+#SBATCH --output=serial_test_%j.log   # Standard output and error log
+
 FILES="./fastqFiles/*"
 counter=0
 
