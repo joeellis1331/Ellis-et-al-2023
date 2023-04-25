03f74d5d27470b3e6524c92980c3d4324c2713fb
Joseph Ellis <61436184+joeellis1331@users.noreply.github.com>
2023-04-25 09:27:53 -0400

    Update rMATS_example_script.sh

--- rMATS_example_script.sh
+++ rMATS_example_script.sh
@@ -8,6 +8,8 @@
 #SBATCH --time=08:00:00                 # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log     # Standard output and error log
 
+module load mats
+
 rmats.py --b1 ./b1_b2_files/dox0.txt --b2 ./b1_b2_files/dox100.txt --gtf Homo_sapiens.GRCh38.101.gtf\
  --od ./results/d0_vs_d100_HEK --tmp ./temp/temp_d0_d100_HEK -t paired --readLength 101 --libType fr-firststran
  
